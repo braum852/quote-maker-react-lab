3c994cd22f440745e6b005182f3e312b824a26b0
braum852 <julian.teo59@yahoo.com>
2022-03-29 13:35:39 +1000

    Done

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,37 @@
 // TODO: Create action creators as defined in tests
+export const addQuote = quote => {
+  return {
+    type: 'ADD_QUOTE',
+    quote: Object.assign({}, quote, { votes: 0 })
+  }
+}
+
+export const removeQuote = quoteId => {
+  return {
+    type: 'REMOVE_QUOTE',
+    quoteId
+  }
+}
+
+export const upvoteQuote = quoteId => {
+  return {
+    type: 'UPVOTE_QUOTE',
+    quoteId
+  }
+}
+
+export const downvoteQuote = quoteId => {
+  return {
+    type: 'DOWNVOTE_QUOTE',
+    quoteId
+  }
+}
+
+//addQuote(quote: {})
+ //     ✓ should return an Object with a type of "ADD_QUOTE" and a quote object
+ //   removeQuote(quoteId: Integer)
+ //     ✓ should return an Object with a type of "REMOVE_QUOTE" and a quoteId
+ //   upvoteQuote(quoteId: Integer)
+ //     ✓ should return an Object with a type of "UPVOTE_QUOTE" and a quoteId
+  //  downvoteQuote(quoteId: INteger)
+ //     ✓ should return an Object with a type of "DOWNVOTE_QUOTE" and a quoteId
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,46 @@
 export default (state = [], action) => {
-  return state;
+  let index;
+  let quote;
+
+  switch (action.type) {
+
+    case 'ADD_QUOTE':
+      return state.concat(action.quote);
+
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId);
+
+    case 'UPVOTE_QUOTE':
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
+
+      return [
+        ...state.slice(0, index),
+        Object.assign({}, quote, { votes: quote.votes += 1 }),
+        ...state.slice(index + 1)
+      ];
+
+    case 'DOWNVOTE_QUOTE':
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
+      if (quote.votes > 0) {
+        return [
+          ...state.slice(0, index),
+          Object.assign({}, quote, { votes: quote.votes -= 1 }),
+          ...state.slice(index + 1)
+        ];
+      }
+      return state;
+
+    default: 
+      return state;
+  }
 }
+
+//Quotes Reducer
+//✓ should return the initial state
+//✓ should handle ADD_QUOTE
+//✓ should handle REMOVE_QUOTE
+//✓ should handle UPVOTE_QUOTE
+//✓ should handle DOWNVOTE_QUOTE and decrement vote count down 1 if vote count is positive
+//✓ should handle DOWNVOTE_QUOTE and do nothing if vote count is 0
